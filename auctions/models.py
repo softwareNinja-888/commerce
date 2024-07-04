7f07cbff90881b80210a2401d443070470860e8d
softwareNinja-888 <mlandvodlamini03@gmail.com>
2024-07-04 13:41:00 +0200

    Add models

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,27 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class AuctionListing(models.Model):
+    items = models.CharField(max_length=64)
+    description = models.CharField(max_length=450)
+    bid = models.IntegerField()
+    img = models.URLField(blank=True)
+    category = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.items}"
+
+class Comment(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    content = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.user} commented: {self.content}"
+    
+class Bids(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.bid} has made a bid of R {self.bid}"
